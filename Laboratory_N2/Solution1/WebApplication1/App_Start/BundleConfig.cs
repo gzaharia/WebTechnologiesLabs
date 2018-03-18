4de6a4fb1dba34ca46b7b3822543f69008de7694
Admin <zaharia.gabi99@gmail.com>
2018-03-18 21:42:57 +0200

    Laboratory_N2

---
+++ Laboratory_N2/Solution1/WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1.Web
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+            bundles.Add(new StyleBundle("~/bundles/footer/css")
+                .Include("~/Content/footer_distributed.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/logo/css")
+                  .Include("~/Content/logo.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/style/css")
+                  .Include("~/Content/style.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/bundles/file/css")
+                  .Include("~/Content/file.css", new CssRewriteUrlTransform()));
+        }
+    }
+   
+}
---
+++ Laboratory_N2/Solution1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Games()
+        {
+            return View();
+        }
+
+        public ActionResult Food()
+        {
+            return View();
+        }
+
+        public ActionResult Music()
+        {
+            return View();
+        }
+
+        public ActionResult Sport()
+        {
+            return View();
+        }
+
+        public ActionResult Technology()
+        {
+            return View();
+        }
+    }
+}
